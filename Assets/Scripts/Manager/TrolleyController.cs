93eeefa7e9638740db0328fcd2a3a07e35302760
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2025-03-19 15:26:01 -0500

    fix: fix start direction, update movement and rotation

--- Assets/Scripts/Manager/TrolleyController.cs
+++ Assets/Scripts/Manager/TrolleyController.cs
@@ -8,7 +8,7 @@ public class TrolleyController : MonoBehaviour
     public LayerMask obstacleLayer;
     public Grid grid;
 
-    private Vector2Int direction = Vector2Int.right;
+    private Vector2Int direction;
     private Transform[] trolleyParts;
     private Transform head;
     private List<Quaternion> partRotations;
@@ -25,54 +25,62 @@ public class TrolleyController : MonoBehaviour
         }
 
         head = trolleyParts[0];
+        direction = GetInitialDirection();
     }
+
+    private Vector2Int GetInitialDirection()
+    {
+        Vector3 forward = head.forward;
+
+        if (Vector3.Dot(forward, Vector3.forward) > 0.5f) return Vector2Int.up;
+        if (Vector3.Dot(forward, Vector3.back) > 0.5f) return Vector2Int.down;
+        if (Vector3.Dot(forward, Vector3.right) > 0.5f) return Vector2Int.right;
+        if (Vector3.Dot(forward, Vector3.left) > 0.5f) return Vector2Int.left;
+
+        return Vector2Int.right;
+    }
+
 
     public void ChangeDirection(Vector2Int newDirection)
     {
         if (newDirection != -direction)
         {
-            direction = newDirection;
+            Vector3Int currentCell = grid.WorldToCell(head.position);
+            Vector3Int newCell = currentCell + new Vector3Int(newDirection.x, 0, newDirection.y);
+            Vector3 newPosition = grid.CellToWorld(newCell);
 
-            head.rotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.y));
-            partRotations[0] = head.rotation;
-
-            MoveTrolley();
-
-            for (int i = 1; i < trolleyParts.Length; i++)
+            if (IsPositionValid(newPosition))
             {
-                if (i == 1)
+                direction = newDirection;
+
+                head.rotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.y));
+                partRotations[0] = head.rotation;
+
+                // Move trolley parts
+                for (int i = trolleyParts.Length - 1; i > 0; i--)
                 {
-                    trolleyParts[i].rotation = head.rotation;
-                    partRotations[i] = head.rotation;
+                    trolleyParts[i].position = trolleyParts[i - 1].position;
+                    partRotations[i] = partRotations[i - 1];
                 }
-                else
+
+                head.DOMove(newPosition, moveDuration);
+
+                // Rotate trolley parts
+                for (int i = 1; i < trolleyParts.Length; i++)
                 {
-                    trolleyParts[i].rotation = partRotations[i];
+                    if (i == 1)
+                    {
+                        trolleyParts[i].rotation = head.rotation;
+                        partRotations[i] = head.rotation;
+                    }
+                    else
+                    {
+                        trolleyParts[i].rotation = partRotations[i];
+                    }
                 }
             }
-
+            else return;
         }
-    }
-
-    private void MoveTrolley()
-    {
-        Vector3Int currentCell = grid.WorldToCell(head.position);
-        Vector3Int newCell = currentCell + new Vector3Int(direction.x, 0, direction.y);
-
-        Vector3 newPosition = grid.CellToWorld(newCell);
-
-        if (IsPositionValid(newPosition))
-        {
-
-            for (int i = trolleyParts.Length - 1; i > 0; i--)
-            {
-                trolleyParts[i].position = trolleyParts[i - 1].position;
-                partRotations[i] = partRotations[i - 1];
-            }
-
-            head.DOMove(newPosition, moveDuration);
-        }
-        else return;
     }
 
     private bool IsPositionValid(Vector3 position)
