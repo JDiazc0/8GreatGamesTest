31e2283981f03cfa0dc6be0c5aee8c9156122433
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2025-03-18 19:29:46 -0500

    feat: create input manager and movement for roaller coaster game

---
+++ Assets/Scripts/Manager/InputManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public LayerMask trolleyLayer;
+    private TrolleyController selectedTrolley;
+    private Vector2 touchStartPosition;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, trolleyLayer))
+            {
+                selectedTrolley = hit.collider.GetComponentInParent<TrolleyController>();
+                if (selectedTrolley != null)
+                {
+                    touchStartPosition = Input.mousePosition;
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0) && selectedTrolley != null)
+        {
+            Vector2 touchEndPosition = Input.mousePosition;
+            Vector2 swipeDirection = touchEndPosition - touchStartPosition;
+
+            if (swipeDirection.magnitude > 20)
+            {
+                Vector2Int newDirection = GetDirectionFromSwipe(swipeDirection);
+                selectedTrolley.ChangeDirection(newDirection);
+            }
+        }
+    }
+
+    private Vector2Int GetDirectionFromSwipe(Vector2 swipe)
+    {
+        if (Mathf.Abs(swipe.x) > Mathf.Abs(swipe.y))
+        {
+            return swipe.x > 0 ? Vector2Int.right : Vector2Int.left;
+        }
+        else
+        {
+            return swipe.y > 0 ? Vector2Int.up : Vector2Int.down;
+        }
+    }
+}
---
+++ Assets/Scripts/Manager/TrolleyController.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using DG.Tweening;
+using System.Collections.Generic;
+
+public class TrolleyController : MonoBehaviour
+{
+    public float moveDuration = 0.2f;
+    public LayerMask obstacleLayer;
+    public Grid grid;
+
+    private Vector2Int direction = Vector2Int.right;
+    private Transform[] trolleyParts;
+    private Transform head;
+    private List<Quaternion> partRotations;
+
+    void Start()
+    {
+        trolleyParts = new Transform[transform.childCount];
+        partRotations = new List<Quaternion>();
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            trolleyParts[i] = transform.GetChild(i);
+            partRotations.Add(trolleyParts[i].rotation);
+        }
+
+        head = trolleyParts[0];
+    }
+
+    public void ChangeDirection(Vector2Int newDirection)
+    {
+        if (newDirection != -direction)
+        {
+            direction = newDirection;
+
+            head.rotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.y));
+            partRotations[0] = head.rotation;
+
+            for (int i = 1; i < trolleyParts.Length; i++)
+            {
+                if (i == 1)
+                {
+                    trolleyParts[i].rotation = head.rotation;
+                    partRotations[i] = head.rotation;
+                }
+                else
+                {
+                    trolleyParts[i].rotation = partRotations[i];
+                }
+            }
+
+            MoveTrolley();
+        }
+    }
+
+    private void MoveTrolley()
+    {
+        Vector3Int currentCell = grid.WorldToCell(head.position);
+        Vector3Int newCell = currentCell + new Vector3Int(direction.x, 0, direction.y);
+
+        Vector3 newPosition = grid.CellToWorld(newCell);
+
+        if (IsPositionValid(newPosition))
+        {
+
+            for (int i = trolleyParts.Length - 1; i > 0; i--)
+            {
+                trolleyParts[i].position = trolleyParts[i - 1].position;
+                partRotations[i] = partRotations[i - 1];
+            }
+
+            head.DOMove(newPosition, moveDuration);
+        }
+        else return;
+    }
+
+    private bool IsPositionValid(Vector3 position)
+    {
+        Collider[] colliders = Physics.OverlapSphere(position, 0.1f, obstacleLayer);
+        return colliders.Length == 0;
+    }
+}
